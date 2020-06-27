4c84c777b1a40d08b79a1225ccd900478d6cea37
JP Ungaretti <hello@jungaretti.com>
2020-06-27 16:13:17 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, request, render_template
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
+# go get values
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load the values from environmental variables, how magical!
+COGSVCS_KEY = os.getenv('COGSVCS_KEY')
+COGSVCS_CLIENTURL = os.getenv('COGSVCS_CLIENTURL')
+
+# Create the core Flask app
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        # User is requesting the form
+        return render_template('form.html')
+    elif request.method == 'POST':
+        # User has sent us data
+        image = request.files['image']
+        client = ComputerVisionClient(
+            COGSVCS_CLIENTURL,
+            CognitiveServicesCredentials(COGSVCS_KEY)
+        )
+        result = client.describe_image_in_stream(image)
+        message = 'No dog found. How sad. :-('
+        if 'dog' in result.tags:
+            message = 'Dog found :D'
+        return render_template('result.html', message=message)
+        
