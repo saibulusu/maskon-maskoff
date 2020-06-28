46d62a3434d68281ab6b6bc5de8ba813347407b1
JP Ungaretti <hello@jungaretti.com>
2020-06-28 13:08:24 -0500

    Connect to CosmosDB

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
-import cv2 as cv2
+import uuid
+# import cv2 as cv2
+from cv2 import cv2
 import numpy as np
 from base64 import b64encode
 from flask import Flask, request, render_template
+from easydict import EasyDict as edict
 
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.face import FaceClient
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
 
-# from FaceMaskDetection.load_model.tensorflow_loader import *
+# Credit: https://github.com/AIZOOTech/FaceMaskDetection
 from FaceMaskDetection.tensorflow_infer import inference
 
 from PIL import Image
@@ -19,19 +23,45 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load the values from environmental variables, how magical!
-COGSVCS_KEY = os.getenv('COGSVCS_KEY')
-COGSVCS_CLIENTURL = os.getenv('COGSVCS_CLIENTURL')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-UPLOAD_FOLDER = '/'
+
+endpoint = os.getenv('COSMOS_URI')
+key = os.getenv('COSMOS_KEY')
+
+client = CosmosClient(endpoint, key)
+
+database_name = 'db_Masks'
+database = client.create_database_if_not_exists(id=database_name)
+
+container_name = 'Tasks'
+container = database.create_container_if_not_exists(
+    id=container_name, 
+    partition_key=PartitionKey(path="/city"),
+    offer_throughput=400
+)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def get_city_name(argument):
+    if argument == "hong-kong":
+        return "Hong Kong"
+    elif argument == "london":
+        return "London"
+    elif argument == "madison":
+        return "Madison"
+    elif argument == "new-delhi":
+        return "New Delhi"
+    elif argument == "paris":
+        return "Paris"
+    elif argument == "seattle":
+        return "Seattle"
+
 # Create the core Flask app
 app = Flask(__name__)
-app.config['TESTING']=True
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+item_id = 1
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,19 +71,16 @@ def index():
     elif request.method == 'POST':
         # User has sent us data
         image = request.files['image']
+        city = request.form.get('cities')
         if image.filename == '' or not allowed_file(image.filename):
             # Bad input
             return render_template('error.html')
-        
-        # face_client = FaceClient(COGSVCS_CLIENTURL, CognitiveServicesCredentials(COGSVCS_KEY))
-        # detected_faces = face_client.face.detect_with_stream(image, detection_model='detection_02')
 
         filestr = image.read()
         #convert string data to numpy array
         npimg = np.fromstring(filestr, np.uint8)
         # convert numpy array to image
         img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         output = inference(img, show_result=False, target_shape=(260, 260))
 
@@ -63,8 +90,11 @@ def index():
             if face[0] == 0:
                 mask_count += 1
 
-        encoded = b64encode(img)
-        mime = "image/jpg"
-        uri = "data:%s;base64,%s" % (mime, encoded)
+        face_count = len(output)
+        if face_count > 0:
+            non_mask_count = face_count - mask_count
+            score = non_mask_count / face_count
+            json_data = edict({'id': str(uuid.uuid4()), 'city': city, 'face_count': face_count, 'mask_count': mask_count, 'non_mask_count': non_mask_count, 'score': score})
+            container.create_item(body=json_data)
 
-        return render_template('result.html', face_count=len(output), mask_count=mask_count, uri=uri)
+        return render_template('result.html', city=get_city_name(city), face_count=len(output), mask_count=mask_count)
